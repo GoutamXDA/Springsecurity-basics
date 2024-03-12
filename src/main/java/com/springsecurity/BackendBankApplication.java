d3587b93908fd6e2cd07b8aecc5b4c22a87a771f
Goutam <goutambyte@gmail.com>
2024-03-12 14:39:03 +0530

    security feature for end points

--- src/main/java/com/springsecurity/BackendBankApplication.java
+++ src/main/java/com/springsecurity/BackendBankApplication.java
@@ -2,10 +2,8 @@ package com.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.springsecurity.controller")
 public class BackendBankApplication {
 
 	public static void main(String[] args) {
