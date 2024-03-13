1d2f0316265ede818007cac6d2933331e18a8ba5
Goutam <goutambyte@gmail.com>
2024-03-13 20:39:55 +0530

    security feature

--- src/main/java/com/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/config/SecurityConfig.java
@@ -11,10 +11,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-		http.formLogin();
-		http.httpBasic();
+		http.authorizeHttpRequests()
+		//need auhtentification i'e login to access these resources(end points)
+		.requestMatchers("/myAccount","/myBlance","/myLoans","/myCards").authenticated()
+		//no auhtentification require to access below resource
+		.requestMatchers("/notices","/contact").permitAll()
+		.and().formLogin()
+		.and().httpBasic();
 		return http.build();
 	}
 
-	}
+}
