3281c4a8a8951322dbbb8c876de60deb663707e6
Goutam <goutambyte@gmail.com>
2024-03-13 20:26:21 +0530

    default security chain filter

--- src/main/java/com/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -14,4 +16,5 @@ public class SecurityConfig {
 		http.httpBasic();
 		return http.build();
 	}
-}
+
+	}
